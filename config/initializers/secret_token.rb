064e35788ace6f9ef27794d7359a90018853a7d1
Mat Schaffer <mat@schaffer.me>
2012-09-05 14:22:08 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Plusdollar::Application.config.secret_token = 'b4cbcf118c00657f9a31b16bd4c19b5091e33bf0f986e077d119e1fd74fc5f1c586af24dc5e9b8579caee1ed94e706d0f2a0cf806cd1c0db7b3eaea5e8a49d93'
