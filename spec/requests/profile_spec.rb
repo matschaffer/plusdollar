3642af63da84059926c8ac072fc26e543d740f94
Mat Schaffer <mat@schaffer.me>
2012-12-09 23:09:34 -0500

    Starting on in-page credit card posting

--- spec/requests/profile_spec.rb
+++ spec/requests/profile_spec.rb
@@ -5,6 +5,13 @@ describe "A users profile", js: true do
     sign_in
     visit profile_path
     click_on "add payment info"
+
+    fill_in "Card number", with: "4242 4242 4242 4242"
+    fill_in "Expiration", with: "12/99"
+    fill_in "Name on card", with: "Mat"
+    fill_in "Card code", with: "123"
+
+    click_on "Save card"
   end
 
   it "links to remove payment info if we have it" do
