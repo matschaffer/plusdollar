7d3e50b06ed7c90f3ad931dff2374ca1a7d56277
Mat Schaffer <mat@schaffer.me>
2012-12-11 10:00:57 -0500

    Use test keys in prod so we can demo without real CC numbers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,10 +73,10 @@ Plusdollar::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  
+
   config.action_mailer.default_url_options = { :host => 'mat-plusdollar.herokuapp.com' }
-  
-  config.stripe_private = "k5nqEdVkbf8ZFv64tbc80AHezT6ifLSF"
-  config.stripe_public = "pk_k9Z9KurLX98Rt0mV9hObleFCs2V60"
+
+  # config.stripe_private = "k5nqEdVkbf8ZFv64tbc80AHezT6ifLSF"
+  # config.stripe_public = "pk_k9Z9KurLX98Rt0mV9hObleFCs2V60"
 
 end
